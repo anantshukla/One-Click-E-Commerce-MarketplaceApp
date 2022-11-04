2984d90664318b9b7a5d12438233de87f84b3f4d
Anant Shukla <accounts@anantshukla.com>
2022-11-04 18:33:18 -0400

    Created API for Create User

--- backend-one-click-classifieds/app.js
+++ backend-one-click-classifieds/app.js
@@ -1,27 +1,102 @@
 const express = require("express");
 const app = express();
 
-const port = 3001
+const port = 3001;
+const defaultErrorStatusCode = 400;
+const defaultSuccessStatusCode = 200;
 
-function successMessage(msg) {
-    return success = {
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    successAPIResponse(req, res, 'One Click Classifieds Rest API is running')
+})
+
+app.post('/createUser', async (req, res) => {
+    email = req.body.email;
+    password = req.body.password;
+    firstName = req.body.firstName;
+    lastName = req.body.lastName;
+    phoneNumber = req.body.phoneNumber;
+
+    
+    emailList = await knex('USERS').select('*').where({email: email})
+    phoneList = await knex('USERS').select('*').where({phone: phoneNumber})
+
+    // Validate whether Email or Phone Number exists
+    if(emailList.length !== 0 || phoneList.length !== 0) {
+        console.log('Phone number or email address already exists');
+        failureAPIResponse(req, res, 'Phone number or email address already exists');
+        return;
+    }
+    else {
+        // Validate and make sure that phoneNumber, lastName, firstName, password, email are not empty
+        if(email === null, password === null, firstName  === null, lastName === null, phoneNumber === null) {
+            failureAPIResponse(req, res, 'Input Fields Cannot be empty');
+        }
+        else {
+            let insertResponse = await knex.insert({
+                email: email,
+                password: password,
+                first_name: firstName,
+                last_name: lastName,
+                phone: phoneNumber,
+            }).into('USERS').returning('id');
+
+            if(insertResponse.length === 0) {
+                failureAPIResponse(req, res, 'Failure in creating user profile');
+            }
+            else {
+                successAPIResponse(req, res, 'User Created successfully');
+            }
+        }
+    }
+})
+  
+app.listen(port, () => {
+    console.log(`One Click Classifieds Backend application is listening on port ${port}`)
+})
+
+
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+      filename: "../sql/one_click_classifieds.sqlite"
+    },
+    useNullAsDefault: true,
+});
+
+
+const successMessage = (msg) => {
+    return {
         message: msg,
         status: 1
     }
 }
 
-function failureMessage(msg) {
-    return success = {
+const failureMessage = (msg) => {
+    return {
         message: msg,
         status: 0
     }
 }
 
+const successAPIResponse = (req, res, msg, statusCode=defaultSuccessStatusCode) => {
+    try {
+        // console.log(msg)
+        res.status(statusCode).json(successMessage(msg));
+    }
+    catch(err) {
+        console.log("Exception while sending success response to user in API " + req.url);
+        console.log(err);
+    }
+};
 
-app.get('/', (req, res) => {
-    res.json(successMessage('One Click Classifieds Rest API is running'));
-})
-  
-  app.listen(port, () => {
-    console.log(`One Click Classifieds Backend application is listening on port ${port}`)
-  })
+const failureAPIResponse = (req, res, msg, statusCode=defaultErrorStatusCode) => {
+    try {
+        res.status(statusCode).json(failureMessage(msg))
+    }
+    catch(err) {
+        console.log("Error: " + msg + " in request: " + req.url);
+    }
+}
