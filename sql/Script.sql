5213730d76ac534564785811133dc5d234ddaf28
Anant Shukla <accounts@anantshukla.com>
2022-11-06 13:22:07 -0500

    Added advertisement table

--- sql/Script.sql
+++ sql/Script.sql
@@ -6,3 +6,16 @@ CREATE TABLE USERS (
 	last_name TEXT NOT NULL,
 	phone TEXT NOT NULL UNIQUE
 );
+
+
+CREATE TABLE ADVERTISEMENTS (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	advertisement_name TEXT NOT NULL,
+	description TEXT,
+	price DECIMAL(10,5) NOT NULL,
+	location TEXT,
+	advertisement_status BOOLEAN DEFAULT 1 NOT NULL, --0 inactive, 1 active
+	seller_id INTEGER,
+	categories TEXT,
+	CONSTRAINT advertisement_user_sellerid FOREIGN KEY (seller_id) REFERENCES USERS(id)
+);
