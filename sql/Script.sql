8824430d38e5600850be0b32e370be91eedfc75b
Anant Shukla <accounts@anantshukla.com>
2022-11-08 14:20:24 -0500

    Updated SQL with the new project

--- sql/Script.sql
+++ sql/Script.sql
@@ -1,21 +1,58 @@
+-- Table for Storing User Details
 CREATE TABLE USERS (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	email TEXT NOT NULL UNIQUE,
 	password TEXT NOT NULL,
 	first_name TEXT NOT NULL,
 	last_name TEXT NOT NULL,
-	phone TEXT NOT NULL UNIQUE
+	phone TEXT NOT NULL UNIQUE,
+	
+	-- For Audit Trailing	
+	created_on DATETIME,
+	created_by TEXT,
+	modified_on DATETIME,
+	modified_by TEXT
+);
+
+-- Table for Storing Product Inventory Details
+CREATE TABLE INVENTORY (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	quantity INTEGER,
+
+	-- For Audit Trailing	
+	created_on DATETIME,
+	created_by TEXT,
+	modified_on DATETIME,
+	modified_by TEXT
+);
+
+-- Table for Storing Product Category Details
+CREATE TABLE CATEGORIES(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT,	
+
+	-- For Audit Trailing	
+	created_on DATETIME,
+	created_by TEXT,
+	modified_on DATETIME,
+	modified_by TEXT
 );
 
 
-CREATE TABLE ADVERTISEMENTS (
+-- Table for Storing Product Details
+CREATE TABLE PRODUCTS (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	advertisement_name TEXT NOT NULL,
+	name TEXT NOT NULL,
 	description TEXT,
 	price DECIMAL(10,5) NOT NULL,
-	location TEXT,
-	advertisement_status BOOLEAN DEFAULT 1 NOT NULL, --0 inactive, 1 active
-	seller_id INTEGER,
-	categories TEXT,
-	CONSTRAINT advertisement_user_sellerid FOREIGN KEY (seller_id) REFERENCES USERS(id)
+	inventory_id INTEGER,
+	category_id INTEGER,
+	-- For Audit Trailing	
+	created_on DATETIME,
+	created_by TEXT,
+	modified_on DATETIME,
+	modified_by TEXT,
+	
+	CONSTRAINT products_inventory_fk FOREIGN KEY (inventory_id) REFERENCES INVENTORY(id),
+	CONSTRAINT products_catogory_fk FOREIGN KEY (category_id) REFERENCES CATEGORIES(id)
 );
