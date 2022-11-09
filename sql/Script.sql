bd817a5b3904bf70445e7aca9391acf1e0695375
Anant Shukla <accounts@anantshukla.com>
2022-11-08 21:02:12 -0500

    Adding new db changes

--- sql/Script.sql
+++ sql/Script.sql
@@ -14,17 +14,6 @@ CREATE TABLE USERS (
 	modified_by TEXT
 );
 
--- Table for Storing Product Inventory Details
-CREATE TABLE INVENTORY (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	quantity INTEGER,
-
-	-- For Audit Trailing	
-	created_on DATETIME,
-	created_by TEXT,
-	modified_on DATETIME,
-	modified_by TEXT
-);
 
 -- Table for Storing Product Category Details
 CREATE TABLE CATEGORIES(
@@ -45,7 +34,6 @@ CREATE TABLE PRODUCTS (
 	name TEXT NOT NULL,
 	description TEXT,
 	price DECIMAL(10,5) NOT NULL,
-	inventory_id INTEGER,
 	category_id INTEGER,
 	-- For Audit Trailing	
 	created_on DATETIME,
@@ -53,6 +41,5 @@ CREATE TABLE PRODUCTS (
 	modified_on DATETIME,
 	modified_by TEXT,
 	
-	CONSTRAINT products_inventory_fk FOREIGN KEY (inventory_id) REFERENCES INVENTORY(id),
 	CONSTRAINT products_catogory_fk FOREIGN KEY (category_id) REFERENCES CATEGORIES(id)
 );
